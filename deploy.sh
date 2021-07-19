cfc44eb02f0e46c454551fcc4f3fa923c2a8f1a1
Ben Chou <ben7152000@gmail.com>
2021-07-20 00:21:14 +0800

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/ben7152000/shopping.git main:gh-pages
+git push -f https://github.com/ben7152000/shopping.git master:gh-pages
 
 cd -
